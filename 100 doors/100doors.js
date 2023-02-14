6dc5fa25434c36ab57dcad40aa30302c136002fd
raskolnikofa <raskolnikofa@icloud.com>
2023-02-14 12:34:52 +0100

    dirty version to have it solved on basic level

--- 100 doors/100doors.js
+++ 100 doors/100doors.js
@@ -1,2 +1,33 @@
-console.log('100 doors');
-console.log('10 doors');
+const openDoor = '@';
+const closedDoor = '#';
+const doorsAmount = 100;
+
+// using for loop
+
+let doors = [];
+
+for (let pass = 1; pass <= 3; pass ++){
+    console.log(pass);
+    if (pass === 1) {
+        for (let door = 1; door <= doorsAmount; door++) {
+            doors.push(openDoor);
+        }
+        console.log('All doors are open now...');
+        console.log(doors.join(''));
+    } else if (pass === 2) {
+        for (let door = 1; door <= doorsAmount; door++) {
+            if (door % 2 === 0) doors[door-1] = closedDoor;
+        }
+        console.log('Every other door is closed now...');
+        console.log(doors.join(''));
+    } else if (pass === 3) {
+        for (let door = 1; door <= doorsAmount; door++) {
+            if (door % 3 === 0) {
+                if (doors[door-1] === openDoor) doors[door-1] = closedDoor;
+                if (doors[door-1] === closedDoor) doors[door-1] = openDoor;
+            }
+        }
+        console.log('Every third door is toggled now...');
+        console.log(doors.join(''));
+    }
+}
