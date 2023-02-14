e206456d9a7350099b6774e43633f8796553fddd
raskolnikofa <raskolnikofa@icloud.com>
2023-02-14 13:39:18 +0100

    jest version with passing tests

--- 100 doors/100doors.js
+++ 100 doors/100doors.js
@@ -1,35 +1,79 @@
 const openDoor = '@';
 const closedDoor = '#';
+const holdingDoor = 'H';
 const doorsAmount = 100;
-
-// using for loop
 
 let doors = [];
 
-for (let pass = 1; pass <= 3; pass ++){
-    console.log(pass);
-    if (pass === 1) {
+// using for loop - dirty version
+// for (let pass = 1; pass <= 3; pass ++){
+//     console.log(pass);
+//     if (pass === 1) {
+//         for (let door = 1; door <= doorsAmount; door++) {
+//             doors.push(openDoor);
+//         }
+//         console.log('All doors are open now...');
+//         console.log(doors.join(''));
+//     } else if (pass === 2) {
+//         for (let door = 1; door <= doorsAmount; door++) {
+//             if (door % 2 === 0) doors[door-1] = closedDoor;
+//         }
+//         console.log('Every other door is closed now...');
+//         console.log(doors.join(''));
+//     } else if (pass === 3) {
+//         for (let door = 1; door <= doorsAmount; door++) {
+//             if (door % 3 === 0) {
+//                 if (doors[door-1] === openDoor) doors[door-1] = closedDoor;
+//                 if (doors[door-1] === closedDoor) doors[door-1] = openDoor;
+//             }
+//         }
+//         console.log('Every third door is toggled now...');
+//         console.log(doors.join(''));
+//     }
+// }
+
+// version for jest testing
+const firstIteration = (doors) => {
         for (let door = 1; door <= doorsAmount; door++) {
             doors.push(openDoor);
         }
-        console.log('All doors are open now...');
-        console.log(doors.join(''));
-    } else if (pass === 2) {
-        for (let door = 1; door <= doorsAmount; door++) {
-            if (door % 2 === 0) doors[door-1] = closedDoor;
-        }
-        console.log('Every other door is closed now...');
-        console.log(doors.join(''));
-    } else if (pass === 3) {
-        for (let door = 1; door <= doorsAmount; door++) {
-            if (door % 3 === 0) {
-                if (doors[door-1] === openDoor) doors[door-1] = closedDoor;
-                if (doors[door-1] === closedDoor) doors[door-1] = openDoor;
+        console.log(`All doors are open now... ${doors.join('')}`);
+        return doors.join('');
+};
+
+const secondIteration = (doors) => {
+    for (let door = 1; door <= doorsAmount; door++) {
+        if (door % 2 === 0) doors[door-1] = closedDoor;
+    }
+    console.log(`Every other door is closed now... ${doors.join('')}`);
+    return doors.join('');
+};
+
+const thirdIteration = (doors) => {
+    for (let door = 1; door <= doorsAmount; door++) {
+        if (door % 3 === 0) {
+            if (doors[door-1] === openDoor) {
+                doors[door - 1] = holdingDoor
+            }
+            else if (doors[door-1] === holdingDoor) {
+                doors[door - 1] = closedDoor
+            }
+            else if (doors[door-1] === closedDoor) {
+                doors[door - 1] = openDoor
             }
         }
-        console.log('Every third door is toggled now...');
-        console.log(doors.join(''));
     }
-}
+    console.log(`Every third door is toggled now... ${doors.join('')}`);
+    return doors.join('');
+};
 
-module.exports = doors;
+firstIteration(doors);
+secondIteration(doors);
+thirdIteration(doors);
+
+module.exports = {
+    doorsAmount,
+    firstIteration,
+    secondIteration,
+    thirdIteration
+};
--- 100 doors/100doors.spec.js
+++ 100 doors/100doors.spec.js
@@ -1,7 +1,30 @@
-const doors = require('./100doors.js')
+const {
+    doorsAmount,
+    firstIteration,
+    secondIteration,
+    thirdIteration
+} = require('./100doors.js')
+
+let doors = [];
+
+const expectedResultFromTheFirstIteration = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+const expectedResultFromTheSecondIteration = "@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#";
+const expectedResultFromTheThirdIteration = "@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#@@@#H#";
 
 describe("Doors", () => {
-    it("should have length of 100", () => {
-        expect(doors).toHaveLength(100);
+    it("should have amount of 100", () => {
+        expect(doorsAmount).toStrictEqual(100);
+    });
+
+    it("should have expected result for the first iteration", () => {
+        expect(firstIteration(doors)).toStrictEqual(expectedResultFromTheFirstIteration);
+    });
+
+    it("should have expected result for the second iteration", () => {
+        expect(secondIteration(doors)).toStrictEqual(expectedResultFromTheSecondIteration);
+    });
+
+    it("should have expected result for the third iteration", () => {
+        expect(thirdIteration(doors)).toStrictEqual(expectedResultFromTheThirdIteration);
     });
 });
